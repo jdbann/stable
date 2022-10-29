54fec9d5f877a81de93091f32cce44214887aac6
John Bannister <johnb@hey.com>
2022-10-29 10:26:15 +0100

    Set explicit spacing scale

--- tailwind.config.js
+++ tailwind.config.js
@@ -89,6 +89,15 @@ module.exports = {
     screens: {
       lg: "1024px",
     },
+    spacing: {
+      0: 0,
+      1: "0.25rem",
+      2: "0.5rem",
+      4: "1rem",
+      6: "1.5rem",
+      8: "2rem",
+      12: "3rem",
+    },
   },
   plugins: [
     plugin(function ({ matchUtilities }) {
