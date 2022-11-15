1e29c3470ed1a468d1c6c863853347aa52563a5e
John Bannister <johnb@hey.com>
2022-11-15 09:51:47 +0000

    Load list of posts and shown on /posts

--- cypress/e2e/posts.cy.ts
+++ cypress/e2e/posts.cy.ts
@@ -1,6 +1,17 @@
+import posts from "../fixtures/posts.json";
+
 describe("posts page", () => {
-  it("loads", () => {
+  it("loads the list of posts", () => {
     cy.visit("/posts");
-    cy.get("h1").contains("Posts");
+
+    cy.get("h1").should("contain.text", "Posts");
+
+    posts.forEach((post) => {
+      cy.get(`article[data-slug='${post.slug}']`).should(($article) => {
+        expect($article.find("h2")).to.contain(post.title);
+        expect($article.find("time")).to.contain(post.created_at);
+        expect($article.find("p")).to.contain(post.summary);
+      });
+    });
   });
 });
